a3ffe2921e941a81a28c00a4b5b40d1fc06f4d90
LKubner <luciano.2022310952@aluno.iffar.edu.br>
2024-07-15 15:44:57 -0300

    .

--- nova_senha.php
+++ nova_senha.php
@@ -4,3 +4,27 @@
 //verificar o token
 $email = $_GET['email'];
 $token = $_GET['token'];
+
+require_once "conexao.php";
+$conexao = conectar();
+$sql = "SELECT * FROM recuperar_senha WHERE email='$email' AND token ='$token'";
+$resultado = executarSQL($conexao, $sql);
+$recuperar = mysqli_fetch_assoc($resultado);
+
+if ($recuperar == null) {
+    echo "Email ou token incorreto. tente fazer um novo pedido de recuperação de senha.";
+    die();
+} else {
+    // verificar a validade do pedido
+    // verificar se o link ja foi usado
+    date_default_timezone_set('America/Sao_Paulo');
+    $data = new DateTime('now');
+    $data_criacao = DateTime::createFromFormat(
+        'Y-m-d H:i:s',
+        $recuperar['data_criacao']
+    );
+    $UmDia = DateInterval::createFromDateString('1 day');
+    $dataExpiracao = date_add($data_criacao, $UmDia);
+    var_dump($data_criacao);
+    var_Dump($dataExpiracao);
+}
